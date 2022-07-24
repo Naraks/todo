2bdbd5dac5da7446f2c04e1508313d779e4aa81c
s.rudenko <s.rudenko.sis@gmail.com>
2022-07-24 13:02:32 +0700

    add task entity. close #2

---
+++ src/main/kotlin/ru/denko/todo/entities/Task.kt
@@ -0,0 +1,36 @@
+package ru.denko.todo.entities
+
+import java.time.LocalDateTime
+import javax.persistence.*
+
+/**
+ * Задача
+ */
+
+@Entity
+class Task(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    var id: Long? = null,
+
+    /**
+     * Краткое описание задачи
+     */
+    @Column(name = "name", nullable = false, length = 100)
+    var name: String,
+
+    /**
+     * Полное описание задачи
+     */
+    @Column(name = "description", nullable = true, length = 2000)
+    var description: String,
+
+    /**
+     * Дата создания
+     */
+    @Column(name = "created_on", nullable = false)
+    val createdOn: LocalDateTime
+
+)
---
+++ src/main/kotlin/ru/denko/todo/repos/TaskRepository.kt
@@ -0,0 +1,7 @@
+package ru.denko.todo.repos
+
+import org.springframework.data.repository.CrudRepository
+import ru.denko.todo.entities.Task
+
+interface TaskRepository: CrudRepository<Task, Long> {
+}
