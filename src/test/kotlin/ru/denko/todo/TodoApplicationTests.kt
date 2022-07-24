72815d111cf7d6dfaad560a193fca76ccfc3eab5
s.rudenko <s.rudenko.sis@gmail.com>
2022-07-24 12:23:01 +0700

    Initial commit

---
+++ src/test/kotlin/ru/denko/todo/TodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.denko.todo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
