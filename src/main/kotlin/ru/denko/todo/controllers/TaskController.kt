3b7700150bdc31fd0895438dad347088fef0d157
s.rudenko <s.rudenko.sis@gmail.com>
2022-07-24 13:41:34 +0700

    task controller fix

--- src/main/kotlin/ru/denko/todo/controllers/TaskController.kt
+++ src/main/kotlin/ru/denko/todo/controllers/TaskController.kt
@@ -5,17 +5,18 @@ import ru.denko.todo.models.TaskModel
 import ru.denko.todo.services.TaskService
 
 @RestController
+@RequestMapping(path = ["/task"])
 class TaskController(
     private val service: TaskService
 ) {
 
-    @GetMapping("/tasks")
+    @GetMapping("/all")
     fun getAllTasks(): List<TaskModel> = service.getAllTasks()
 
-    @GetMapping("/task/{id}")
+    @GetMapping("/{id}")
     fun getTask(@PathVariable id: String): TaskModel = service.getTask(id.toLong())
 
-    @PostMapping("/task/create")
+    @PostMapping("/create")
     fun createTask(@RequestBody taskModel: TaskModel) = service.createTask(taskModel)
 
 }
